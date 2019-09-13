cd58cd55199728184d88e5f65f7abc02cc968625
C3NZ <cenz@cenz.io>
2019-09-12 19:49:37 -0700

    [add] a database connection

--- application.py
+++ application.py
@@ -1,3 +1,6 @@
+import os
+
+import MySQLdb as sql
 import numpy as np
 import tensorflow as tf
 from flask import (Flask, abort, jsonify, make_response, redirect,
@@ -7,6 +10,28 @@ from keras.models import load_model
 from keras.preprocessing.image import img_to_array
 from PIL import Image
 from werkzeug.datastructures import FileStorage
+
+# Setup db connection
+if "RDS_HOSTNAME" in os.environ:
+    DB = {
+        "NAME": os.environ["RDS_DB_NAME"],
+        "USER": os.environ["RDS_USERNAME"],
+        "PASSWORD": os.environ["RDS_PASSWORD"],
+        "HOST": os.environ["RDS_HOSTNAME"],
+        "PORT": os.environ["RDS_PORT"],
+    }
+
+    connection = sql.connect(
+        host=DB["HOST"],
+        user=DB["USER"],
+        password=DB["PASSWORD"],
+        port=DB["PORT"],
+        db=DB["name"],
+    )
+
+    cursor = connection.cursor()
+    cursor.execute('CREATE TABLE Info (ID)')
+
 
 graph = tf.get_default_graph()
 application = app = Flask(__name__)
@@ -35,6 +60,11 @@ class Mnist(Resource):
     @api.doc(parser=single_parser, description="Enter an image")
     def post(self):
         """Uploads a new transaction to Rex (Click to see more)"""
+        if connection:
+            cursor = connection.cursor()
+            cursor.
+
+
         args = single_parser.parse_args()
         img: Image = Image.open(args.file)
         img = img.resize((28, 28))
@@ -48,7 +78,6 @@ class Mnist(Resource):
             pred = np.argmax(all_pred[0])
 
         return {"pred": str(pred)}
-
 
 if __name__ == "__main__":
     app.run()
